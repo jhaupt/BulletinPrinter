e1778eb6f3b2cd3edbd3794602028842d530902e
jhaupt <justine.haupt@gmail.com>
2024-05-07 23:38:58 -0400

    changes

--- BulletinPrinter.java
+++ BulletinPrinter.java
@@ -1,35 +1,50 @@
 import java.awt.Font;
 import javax.swing.*;        
- 
-class BulletinPrinter {
-    private static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("BulletinPrinter");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
- 
-        //Add the ubiquitous "Hello World" label.
-        JLabel label = new JLabel("This is a good idea.");
-        frame.getContentPane().add(label);
+import java.io.File;  
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
-	// Set colors and font
-	label.setForeground(new java.awt.Color(255, 255, 255));	
-	label.setFont(new Font("Verdana", Font.BOLD, 20));
-	frame.getContentPane().setBackground(new java.awt.Color(0, 0, 0));
+public class BulletinPrinter {
+
+	public static String readMessage() {
+		String line_of_text = "null";	//Initialize line_of_text
+		try {
+			Scanner myReader = new Scanner(new File("./messages.txt"));
+			while (myReader.hasNextLine()) {
+				line_of_text = myReader.nextLine();
+			}
+			myReader.close();
+		} 
+		catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+
+		return line_of_text;
+	}
+
+	private static void createAndShowGUI() {
+		//Create and set up the window.
+		JFrame frame = new JFrame("BulletinPrinter");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
- 
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
+		//Add the ubiquitous "Hello World" label.
+		JLabel label = new JLabel(readMessage());
+		frame.getContentPane().add(label);
 
-    //add text reader with variable that changes over time 
+		// Set colors and font
+		label.setForeground(new java.awt.Color(255, 255, 255));	
+		label.setFont(new Font("Verdana", Font.BOLD, 20));
+		frame.getContentPane().setBackground(new java.awt.Color(0, 0, 0));
 
 
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-    }
+		//Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		createAndShowGUI();
+	}
 }
